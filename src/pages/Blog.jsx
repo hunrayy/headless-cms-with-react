950068e15fc1db5e0a1844dbc80e1dc118e85b1f
hunrayy <henryokiyi8@gmail.com>
2025-02-13 21:33:36 +0100

    added to blog component

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -78,7 +78,7 @@ const Blog = () => {
                     >
                       Read More
                     </button>
-                  </div>
+                  </div><p>hdjdjd</p>
                 </div>
               ))}
         </main>
